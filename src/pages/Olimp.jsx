4df38a8a0fd388dbd150a5edd40c2b8d2c385c2b
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2022-02-03 08:02:21 +0200

    react form fuctional

--- src/pages/Olimp.jsx
+++ src/pages/Olimp.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Olimp() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ea5w2pg",
+        "template_22xowv9",
+        form.current,
+        "user_3dO0i6OPdpXqoxoHSNrwB"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="olimp">
       <div className="txtBox">
@@ -10,6 +33,18 @@ function Olimp() {
           așteptăm să ne lași o părere în chat.
         </p>
       </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name="nume" placeholder="nume" id="" />
+        <input type="text" name="email" placeholder="email" id="" />
+        <textarea
+          name="mesaj"
+          placeholder="mesaj"
+          id=""
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button>Send email</button>
+      </form>
     </div>
   );
 }
